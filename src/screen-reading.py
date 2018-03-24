0969fd435456eab9e8a8b2e0e781ab819038865f
Anurag Wagh <a9raag@gmail.com>
2018-03-24 17:40:21 +0530

    phase1:[ROI] Added reigon of interest

--- src/screen-reading.py
+++ src/screen-reading.py
@@ -1,10 +1,10 @@
-import numpy as np
-from PIL import ImageGrab
-import cv2
 import time
 
-from directkeys import PressKey, W, A, S, D
+import cv2
+import numpy as np
+from PIL import ImageGrab
 
+from src.directkeys import PressKey, W, A, S, D
 
 
 def screen_record():
@@ -20,8 +20,11 @@ def screen_record():
 def process_img (image):
     processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     processed_img = cv2.Canny(processed_img,threshold1=200, threshold2=300)
+    vertices = np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500]])
 
-    vertices = np.array([10,500],[10,300],[300,200],[500,200],[800,300],[800,500])
+    processed_img = roi(processed_img,[vertices])
+
+    lines = cv2.HoughLines(processed_img,)
     return processed_img
 
 
@@ -29,7 +32,7 @@ def roi(img, vertices):
     # blank mask
     mask = np.zeros_like(img)
     # fill the mask 
-    cv.fillPoly(mask,vertices,255)
+    cv2.fillPoly(mask,vertices,255)
     # now only show the area that is the mask 
     masked = cv2.bitwise_and(img,mask)
     return masked 
@@ -41,7 +44,7 @@ def main():
         time.sleep(1)
     last_time = time.time()
     while True: 
-        PressKey(W)
+##        PressKey(W)
         screen = np.array(ImageGrab.grab(bbox= (0,40,800,600)))
         last_time = time.time()
         new_screen = process_img(screen)
