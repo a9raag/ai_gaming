983801d638b8c57899e59bd41a3c403ea1c2abb9
Anurag Wagh <a9raag@gmail.com>
2018-03-25 12:01:16 +0530

    phase1: draw Hough lines on screen
    Detect hough lines, use Gaussian Blur to improve lane detection

--- src/screen-reading.py
+++ src/screen-reading.py
@@ -7,24 +7,39 @@ from PIL import ImageGrab
 from src.directkeys import PressKey, W, A, S, D
 
 
+def draw_lines(img, lines):
+    try:
+        for line in lines:
+            coords = line[0]
+            cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255, 255, 255], 3)
+    except:
+        pass
+
+
 def screen_record():
     last_time = time.time()
     while (True):
         printscreen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
         print('loop took {} secs'.format(time.time() - last_time))
         last_time = time.time()
-##        cv2.imshow('window', cv32.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        # cv2.imshow('window', cv32.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
         if (cv2.waitKey(25) & 0xFF == ord('q')):
             cv2.destroyAllWindows()
             break
-def process_img (image):
-    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    processed_img = cv2.Canny(processed_img,threshold1=200, threshold2=300)
-    vertices = np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500]])
 
-    processed_img = roi(processed_img,[vertices])
 
-    lines = cv2.HoughLines(processed_img,)
+def process_img(image):
+    processed_img = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # Canny edge detection
+    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    #
+    vertices = np.array([[10, 500], [10, 300], [300, 200], [500, 200], [800, 300], [800, 500]], np.int32)
+    processed_img = cv2.GaussianBlur(processed_img, (5, 5), 0)
+    # DETECT ROIs
+    processed_img = roi(processed_img, [vertices])
+
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi / 180, 180, lines=np.array([]), minLineLength=100, maxLineGap=5)
+    draw_lines(processed_img, lines)
     return processed_img
 
 
@@ -32,25 +47,26 @@ def roi(img, vertices):
     # blank mask
     mask = np.zeros_like(img)
     # fill the mask 
-    cv2.fillPoly(mask,vertices,255)
+    cv2.fillPoly(mask, vertices, 255)
     # now only show the area that is the mask 
-    masked = cv2.bitwise_and(img,mask)
-    return masked 
+    masked = cv2.bitwise_and(img, mask)
+    return masked
 
 
 def main():
     for i in range(4)[::-1]:
-        print(i+1)
+        print(i + 1)
         time.sleep(1)
     last_time = time.time()
-    while True: 
-##        PressKey(W)
-        screen = np.array(ImageGrab.grab(bbox= (0,40,800,600)))
+    while True:
+        # PressKey(W)
+        screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 600)))
         last_time = time.time()
         new_screen = process_img(screen)
-        cv2.imshow('window',new_screen)
+        cv2.imshow('window', new_screen)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
+
 
 main()
